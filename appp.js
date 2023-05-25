1ca36b971dac45003a698c3bddd728d2d4f818c4
kiran B <kirankiranbang@gmail.com>
2023-05-25 19:26:36 +0530

    app.js

---
+++ appp.js
@@ -0,0 +1,25 @@
+
+
+
+const http = require('http');
+const routes = require('./routes.js');
+
+const server = http.createServer(routes);
+
+server.listen(4000);
+
+//or
+
+// const http = require('http');
+// const routes = require('./routes.js');
+// console.log(routes.someText);
+// const server = http.createServer(routes.handler);
+
+// server.listen(4000);
+
+
+
+
+//routes is a file name u have created routes.js
+
+//this file name app.js while running (run node app.js) not a( routes.js file)
